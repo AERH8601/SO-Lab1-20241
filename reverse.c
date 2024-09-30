9b3f68663ed0423a7785a23b2e50592208abe5e1
Andres Rossi <andres.rossi@udea.edu.co>
2024-09-29 23:53:22 -0500

    Modifica reverse para crear verificación archivo input y output, pero retorna error en test 4.

--- reverse.c
+++ reverse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
 test
@@ -13,6 +14,8 @@ test
 
 int main(int argc, char *argv[])
 {
+    FILE *input_file = fopen(argv[1], "file");
+
     // Para test 1: Verificar si hay más de 3 argumentos
     if (argc > 3)
     {
@@ -20,17 +23,27 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // Para test 2: Si se pasa un archivo de entrada, verificar si existe
+    // Para test 4: Verificar si el archivo de entrada y salida son el mismo
+    if (argc == 3 && strcmp(argv[1], argv[2]) == 0)
+    {
+        fprintf(stderr, "reverse: input and output file must differ\n");
+        fclose(input_file);
+        exit(1);
+    }
+
+    // Para test 2 y 3: Si se pasa un archivo de entrada, verificar si existe
     if (argc > 1)
     {
-        FILE *input_file = fopen(argv[1], "file");
+
         if (input_file == NULL)
         {
             // Si no se puede abrir el archivo, mostrar un mensaje de error y salir
             fprintf(stderr, "reverse: cannot open file '%s'\n", argv[1]);
             exit(1);
         }
+
         // Cerrar el archivo después de verificar que se abrió correctamente
+
         fclose(input_file);
     }
 
